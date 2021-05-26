01c4bde664852022137536f85b763b32f037521b
Tyler Shipe <tjshipe@gmail.com>
2021-05-26 18:51:37 -0400

    rustfmt common/src/lib

--- common/src/lib.rs
+++ common/src/lib.rs
@@ -1,15 +1,19 @@
-use std::cell::{RefMut, Ref};
-use bytemuck::{Pod, from_bytes_mut, from_bytes};
+use bytemuck::{from_bytes, from_bytes_mut, Pod};
 use solana_program::account_info::AccountInfo;
 use solana_program::program_error::ProgramError;
+use std::cell::{Ref, RefMut};
 
 pub trait Loadable: Pod {
     fn load_mut<'a>(account: &'a AccountInfo) -> Result<RefMut<'a, Self>, ProgramError> {
         // TODO verify if this checks for size
-        Ok(RefMut::map(account.try_borrow_mut_data()?, |data| from_bytes_mut(data)))
+        Ok(RefMut::map(account.try_borrow_mut_data()?, |data| {
+            from_bytes_mut(data)
+        }))
     }
     fn load<'a>(account: &'a AccountInfo) -> Result<Ref<'a, Self>, ProgramError> {
-        Ok(Ref::map(account.try_borrow_data()?, |data| from_bytes(data)))
+        Ok(Ref::map(account.try_borrow_data()?, |data| {
+            from_bytes(data)
+        }))
     }
 
     fn load_from_bytes(data: &[u8]) -> Result<&Self, ProgramError> {
