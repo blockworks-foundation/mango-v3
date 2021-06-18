4af1317dbd0b560063aca8eabae1c7c898918bff
Maximilian Schneider <mail@maximilianschneider.net>
2021-06-18 13:43:23 -0400

    correctly parse spot order accounts from instruction for health check

--- program/src/processor.rs
+++ program/src/processor.rs
@@ -1183,7 +1183,7 @@ impl Processor {
         order_type: OrderType,
     ) -> MerpsResult<()> {
         const NUM_FIXED: usize = 8;
-        let accounts = array_ref![accounts, 0, NUM_FIXED];
+        let (fixed_accs, open_orders_ais) = array_refs![accounts, NUM_FIXED; ..;];
         let [
             merps_group_ai,     // read
             merps_account_ai,   // write
@@ -1193,7 +1193,7 @@ impl Processor {
             bids_ai,            // write
             asks_ai,            // write
             event_queue_ai,     // write
-        ] = accounts;
+        ] = fixed_accs;
         let merps_group = MerpsGroup::load_checked(merps_group_ai, program_id)?;
 
         let mut merps_account =
@@ -1204,6 +1204,20 @@ impl Processor {
 
         check!(owner_ai.is_signer, MerpsErrorCode::Default)?;
         check_eq!(&merps_account.owner, owner_ai.key, MerpsErrorCode::InvalidOwner)?;
+
+
+        for i in 0..merps_group.num_oracles {
+            if !merps_account.in_basket[i] || merps_group.spot_markets[i].is_empty() {
+                continue;
+            }
+            check_eq!(
+                open_orders_ais[i].key,
+                &merps_account.spot_open_orders[i],
+                MerpsErrorCode::Default
+            )?;
+            check_open_orders(&open_orders_ais[i], &merps_group.signer_key)?;
+        }
+
         // TODO could also make class PosI64 but it gets ugly when doing computations. Maybe have to do this with a large enough dev team
         check!(price > 0, MerpsErrorCode::Default)?;
         check!(quantity > 0, MerpsErrorCode::Default)?;
@@ -1237,7 +1251,7 @@ impl Processor {
             client_order_id,
         )?;
 
-        let health = merps_account.get_health(&merps_group, &merps_cache, &[], HealthType::Init)?;
+        let health = merps_account.get_health(&merps_group, &merps_cache, open_orders_ais, HealthType::Init)?;
         check!(health >= ZERO_I80F48, MerpsErrorCode::InsufficientFunds)?;
 
         Ok(())
