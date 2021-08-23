ac1e14b5b9bfb32e04bd436eedfed63bdc789296
Maximilian Schneider <mail@maximilianschneider.net>
2021-08-23 14:06:15 +0200

    add new instruction to transfer admin cap to governance (#38)

--- program/src/instruction.rs
+++ program/src/instruction.rs
@@ -540,7 +540,7 @@ pub enum MangoInstruction {
     /// Accounts expected by this instruction (3):
     /// 0. `[writable]` mango_group_ai - MangoGroup that this mango account is for
     /// 1. `[writable]` perp_market_ai - PerpMarket
-    /// 2. `[signer]` admin_ai - MangoAccount admin
+    /// 2. `[signer]` admin_ai - MangoGroup admin
     ChangePerpMarketParams {
         maint_leverage: Option<I80F48>,
         init_leverage: Option<I80F48>,
@@ -556,6 +556,14 @@ pub enum MangoInstruction {
         /// amount MNGO rewarded per period
         mngo_per_period: Option<u64>,
     },
+
+    /// Transfer admin permissions over group to another account
+    ///
+    /// Accounts expected by this instruction (3):
+    /// 0. `[writable]` mango_group_ai - MangoGroup that this mango account is for
+    /// 1. `[]` new_admin_ai - New MangoGroup admin
+    /// 2. `[signer]` admin_ai - MangoGroup admin
+    SetGroupAdmin {},
 }
 
 impl MangoInstruction {
@@ -830,6 +838,8 @@ impl MangoInstruction {
                     mngo_per_period: unpack_u64_opt(mngo_per_period),
                 }
             }
+
+            38 => MangoInstruction::SetGroupAdmin {},
 
             _ => {
                 return None;
