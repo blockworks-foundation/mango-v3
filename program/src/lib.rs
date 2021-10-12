a37e8e2eea1382f2354ccd9aef6e5eb2bf9eca8a
Bamboozelino <memail>
2021-10-12 14:29:16 +0800

    Put the mango_program module behind `anchor-cpi` feature

--- program/src/lib.rs
+++ program/src/lib.rs
@@ -5,14 +5,15 @@ pub mod error;
 
 pub mod ids;
 pub mod instruction;
-pub mod mango_program;
 pub mod matching;
 pub mod oracle;
 pub mod processor;
 pub mod queue;
 pub mod state;
 pub mod utils;
-pub use mango_program::Mango;
+
+#[cfg(feature = "anchor-cpi")]
+pub mod mango_program;
 
 #[cfg(not(feature = "no-entrypoint"))]
 pub mod entrypoint;
